5ab0c07468e46998dceaac579edb2eaf8748d937
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2023-08-07 17:46:44 +0000

    Deploying to gh-pages from @ ArtemRysich/Test_group_88@6cb798915f53bbc8fa8a342fabf0f24449002469 🚀

---
+++ index.48c71c1a.js
@@ -0,0 +1,2 @@
+!function(){count=0;for(var o=0;o<10;o+=1)count+=1,console.log(count);test="hello world",console.log(test)}();
+//# sourceMappingURL=index.48c71c1a.js.map
---
+++ index.bd106b9a.js
@@ -0,0 +1,2 @@
+count=0;for(let o=0;o<10;o+=1)count+=1,console.log(count);test="hello world",console.log("hello world");
+//# sourceMappingURL=index.bd106b9a.js.map
